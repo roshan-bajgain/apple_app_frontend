431c6f6f0b96b423f95820a2f3a05f3a0c55ac2b
roshan-bajgain <roshan.bazgain@gmail.com>
2024-03-14 13:29:58 +0545

    Add redux store so that all the updates are done through store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Routes, redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Cart from './components/Cart';
 import Home from './components/Home';
@@ -11,9 +11,10 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path='/' exact element={<Home/>} />
           <Route path='/cart' element={<Cart/>} />
           <Route path='/not-found' element={<NotFound />} />
+          <Route path='*' element={<Navigate to='/not-found' />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ConfigureStore, configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+  reducer: {}
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 

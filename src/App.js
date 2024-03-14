fac7cad6707aacce11e0bc44c96bbe12b57a6a55
roshan-bajgain <roshan.bazgain@gmail.com>
2024-03-14 11:35:58 +0545

    Add route and also create Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter, Route, Routes, redirect } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Roshan</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/cart' element={<Cart/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

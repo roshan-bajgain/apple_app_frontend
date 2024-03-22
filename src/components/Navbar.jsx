4c422a6910a2d18f4e806b3c8b3347fb23482a51
roshan-bajgain <roshan.bazgain@gmail.com>
2024-03-22 08:31:00 +0545

    Fix error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { UseSelector, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {

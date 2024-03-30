5b6b50481b0937e05e6b02fb028a1333dbc2897d
roshan-bajgain <roshan.bazgain@gmail.com>
2024-03-30 16:13:15 +0545

    Fix error

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 import {toast} from "react-toastify";
 
+let emailAddress= localStorage.getItem("emailAddress") ? JSON.parse(localStorage.getItem("emailAddress")): ""
+
 const initialState = {
-    cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")): [],
+    cartItems: localStorage.getItem(`${emailAddress}+"_cartItems"`) ? JSON.parse(localStorage.getItem(`${emailAddress}+"_cartItems"`)): [],
     cartTotalQualtity: 0,
     cartTotalAmount: 0,
     email: localStorage.getItem("emailAddress") ? JSON.parse(localStorage.getItem("emailAddress")): ""
@@ -28,14 +30,14 @@ const cartSlice = createSlice({
                     position: "bottom-left"
                 })
             }   
-            localStorage.setItem("cartItems", JSON.stringify(state.cartItems))
+            localStorage.setItem(`${state.email}+"_cartItems"`, JSON.stringify(state.cartItems))
         },
         removeFromCart(state, action){
             const nextCartItems = state.cartItems.filter(
                 cartItem => cartItem.id !== action.payload.id
             )
             state.cartItems = nextCartItems;
-            localStorage.setItem("cartItems",JSON.stringify(state.cartItems))
+            localStorage.setItem(`${state.email}+"_cartItems"`,JSON.stringify(state.cartItems))
             toast.error(`${action.payload.name} is removed from cart`,{
                 position: "bottom-left"
             })
@@ -58,14 +60,14 @@ const cartSlice = createSlice({
                     position: "bottom-left"
                 })
             }
-            localStorage.setItem("cartItems",JSON.stringify(state.cartItems))
+            localStorage.setItem(`${state.email}+"_cartItems"`,JSON.stringify(state.cartItems))
         },
         clearCart(state, action){
             state.cartItems = [];
             toast.error(`All the cart is cleared`,{
                 position: "bottom-left"
             })
-            localStorage.setItem("cartItems", JSON.stringify(state.cartItems))
+            localStorage.setItem(`${state.email}+"_cartItems"`, JSON.stringify(state.cartItems))
         },
         getTotals(state, action){
             let {total, quantity} = state.cartItems.reduce((cartTotal, cartItem)=>{
